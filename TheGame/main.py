5c5043f1fad96486e08a0ecfbf74464b46610c32
gillop_f <72975101+Sir-Freddy@users.noreply.github.com>
2021-04-30 12:55:27 +0200

    d

--- TheGame/main.py
+++ TheGame/main.py
@@ -3,23 +3,19 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-# grass_texture = load_texture('assets/grass_block.png')
-
 window.fps_counter.enabled = False
 window.exit_button.visible = False
 
 class Voxel(Button):
-    def __init__(self, position = (0,0,0)): # -> def __init__(self, position = (0,0,0), texture = grass_texture):
+    def __init__(self, position = (0,0,0)):
         super().__init__(
             parent = scene,
             position = position,
             model = 'cube',
-            # model = 'assets/block',    -> put a texture of a block made with blender to have the UV map
             origin_y = 0.5,
             texture = 'white_cube', # -> texture = texture,
             color = color.color(0,0,random.uniform(0.9,1)),
             highlight_color = color.light_gray,
-            # scale = 0.5,    -> change this when texture is loaded
         )
     def input(self, key):
         if self.hovered:
@@ -31,7 +27,6 @@ class Voxel(Button):
 for z in range(20):
     for x in range(20):
         voxel = Voxel(position = (x,0,z))
-
 
 player = FirstPersonController()
 
